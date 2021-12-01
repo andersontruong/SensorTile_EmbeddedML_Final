458ed60bdc5091420c3d7b2b145b09c2979c37bd
burgerballs <insanederson@gmail.com>
2021-12-01 00:17:51 -0800

    Updated Random Weight Generator

--- randomweights.py
+++ randomweights.py
@@ -5,7 +5,7 @@ count = int(input('How many weights? '))
 with open('weights.txt', 'w+') as f:
     f.write(f'float weights[{count}] = {{')
     weights = [round(random.random(), 4) for i in range(count)]
-    weights_string = [f'{weight:<08},' if (i+1)%5 != 0 else f'{weight:<08},\n' for i, weight in enumerate(weights)]
+    weights_string = [f'{weight:<08},' if (i+1)%5 != 0 else f'{weight:<08},\n                     ' for i, weight in enumerate(weights)]
 
     csv_string = ' '.join(weights_string)
     csv_string = csv_string.rstrip(',')
